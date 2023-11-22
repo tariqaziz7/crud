5cbd9deab4f1d1dbbc28b49434bfb039403d6839
Tariq A <tariqa@datatemplate.com>
2023-11-22 15:42:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todos from './components/Todos/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { User } from "./User";
+import { AddUser } from "./AddUser";
+import "./styles.css";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
+  };
+
+  const onAdd = async (name, email) => {
+    await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 201) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setUsers((users) => [...users, data]);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onEdit = async (id, name, email) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: name,
+        email: email
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        // setUsers((users) => [...users, data]);
+        const updatedUsers = users.map((user) => {
+          if (user.id === id) {
+            user.name = name;
+            user.email = email;
+          }
+
+          return user;
+        });
+
+        setUsers((users) => updatedUsers);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "DELETE"
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          setUsers(
+            users.filter((user) => {
+              return user.id !== id;
+            })
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="App">
+      <h1>Users</h1>
+      <AddUser onAdd={onAdd} />
+      {users.map((user) => (
+        <User
+          id={user.id}
+          key={user.id}
+          name={user.name}
+          email={user.email}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+}
